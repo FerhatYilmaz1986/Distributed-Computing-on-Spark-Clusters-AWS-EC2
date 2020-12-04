787be03caa5706afb38f3b11bc2ec0567b2d3a25
Fer <yilmazferhat1986@gmail.com>
2020-12-04 16:21:35 -0500

    Task : Update prediction model

--- prediction_model.py
+++ prediction_model.py
@@ -2,8 +2,10 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.session import SparkSession
+from pyspark.ml.classification import RandomForestClassificationModel
 import sys
 
 #Create and connect to spark session, read data given in docker command
 spark = SparkSession.builder.master('local[*]').appName('Predict_model').getOrCreate()
 test_set =spark.read.csv(sys.argv[-1], header=True, inferSchema= True, sep=';')
+
