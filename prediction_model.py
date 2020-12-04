149283406553c93e39ee6d06903158492471ba35
Fer <yilmazferhat1986@gmail.com>
2020-12-04 16:23:25 -0500

    Task : Update prediction model

--- prediction_model.py
+++ prediction_model.py
@@ -3,9 +3,12 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.session import SparkSession
 from pyspark.ml.classification import RandomForestClassificationModel
+from pyspark.ml.feature import VectorAssembler
 import sys
 
 #Create and connect to spark session, read data given in docker command
 spark = SparkSession.builder.master('local[*]').appName('Predict_model').getOrCreate()
 test_set =spark.read.csv(sys.argv[-1], header=True, inferSchema= True, sep=';')
+
+# Create feature vector
 
