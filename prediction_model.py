3c0cf7ce5bddf7ba2dfcbc8baffccf643e24b550
Fer <yilmazferhat1986@gmail.com>
2020-12-04 16:20:07 -0500

    Task : Update prediction model

--- prediction_model.py
+++ prediction_model.py
@@ -2,5 +2,8 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.session import SparkSession
+import sys
 
-
+#Create and connect to spark session, read data given in docker command
+spark = SparkSession.builder.master('local[*]').appName('Predict_model').getOrCreate()
+test_set =spark.read.csv(sys.argv[-1], header=True, inferSchema= True, sep=';')
